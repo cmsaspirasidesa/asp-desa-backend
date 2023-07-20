8722ce04f60dc42a83d79595cdfd33ad60e31cfd
ryvasa <ryanoktaviansaputra9@gmail.com>
2023-07-20 20:24:18 +0700

    feat: create PUT endpoint /users/:id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models').User;
 const Role = require('../models').Role;
 
@@ -25,7 +26,6 @@ exports.findAllUser = async (req, res) => {
   }
 };
 
-
 exports.findUserById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -40,6 +40,49 @@ exports.findUserById = async (req, res) => {
       message: 'Detail data user',
       errors: null,
       data: theUser,
+    };
+    res.status(200).send(response);
+  } catch (error) {
+    const response = {
+      status_response: false,
+      message: error.message,
+      errors: error,
+      data: null,
+    };
+    res.status(500).send(response);
+  }
+};
+
+exports.updateUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findUser = await User.findByPk(id);
+    if (!findUser) {
+      const response = {
+        status_response: false,
+        message: 'User tidak di temukan',
+        errors: 'Not Found',
+        data: null,
+      };
+      return res.status(404).send(response);
+    }
+    const { nama, email, password, alamat, nik } = req.body;
+
+    const data = {
+      nama,
+      email,
+      alamat,
+      nik,
+    };
+    if (password) {
+      data.password = bcrypt.hashSync(password, 8);
+    }
+    const user = await User.update(data, { where: { id } });
+    const response = {
+      status_response: true,
+      message: 'User berhasil di update',
+      errors: null,
+      data: user,
     };
     res.status(200).send(response);
   } catch (error) {
