4cb50d8fa808b9075cb9852a96db76567ef9dce7
ryvasa <ryanoktaviansaputra9@gmail.com>
2023-07-20 13:46:41 +0700

    test gitignore

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { urlencoded } = express;
 // const db = require('./models');
 const authRoute = require('./routes/auth.routes');
+const userRoute = require('./routes/user.routes');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -10,6 +11,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(authRoute);
+app.use(userRoute);
 
 // db.sequelize.sync().then(() => {
 app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
