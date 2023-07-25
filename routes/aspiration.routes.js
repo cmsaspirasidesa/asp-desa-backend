a97c050abd8a01256856991a97a77e6c3c48fc22
MangEs23 <tempestdragon987@gmail.com>
2023-07-25 09:19:45 +0700

    feat: create GET endpoint /aspirations/:id

--- routes/aspiration.routes.js
+++ routes/aspiration.routes.js
@@ -6,14 +6,11 @@ const upload = require('../middleware/multer');
 
 router.post(
   '/aspirations',
-  upload.array('images', 2),
   verifyToken,
+  upload.array('images', 2),
   aspirationController.addAspiration,
 );
-<<<<<<< HEAD
-router.get('/aspirations/:id', aspirationController.getAspirationById);
-=======
 router.get('/aspirations', aspirationController.getAllAspirations);
->>>>>>> f2928d6e5aba3ba24034193864a28a0a6b673ca5
+router.get('/aspirations/:id', aspirationController.getAspirationById);
 
 module.exports = router;
